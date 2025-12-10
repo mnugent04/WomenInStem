8d13137c4e6d491ca87304f01a6b5086e2e03572
Meghan Nugent <mnugent@westmont.edu>
2025-12-10 12:57:51 -0800

    updated schema to auto inc primary keys

--- youthGroupSchema.sql
+++ youthGroupSchema.sql
@@ -1,92 +1,93 @@
-CREATE DATABASE YouthGroupDB;
 
+CREATE DATABASE YouthGroupDB;
 USE YouthGroupDB;
 
 CREATE TABLE Person
 (
-    ID        INT PRIMARY KEY,
+    ID        INT AUTO_INCREMENT PRIMARY KEY,
     FirstName VARCHAR(50) NOT NULL,
     LastName  VARCHAR(50) NOT NULL,
-    Age       INT         NOT NULL
+    Age       INT NOT NULL
 );
 
 CREATE TABLE Volunteer
 (
-    ID       INT PRIMARY KEY,
+    ID       INT AUTO_INCREMENT PRIMARY KEY,
     PersonID INT NOT NULL,
-    FOREIGN KEY (PersonID) REFERENCES Person (ID)
+    FOREIGN KEY (PersonID) REFERENCES Person(ID)
 );
 
 CREATE TABLE Attendee
 (
-    ID       INT PRIMARY KEY,
-    PersonID INT          NOT NULL,
+    ID       INT AUTO_INCREMENT PRIMARY KEY,
+    PersonID INT NOT NULL,
     Guardian VARCHAR(100) NOT NULL,
-    FOREIGN KEY (PersonID) REFERENCES Person (ID)
+    FOREIGN KEY (PersonID) REFERENCES Person(ID)
 );
 
 CREATE TABLE Leader
 (
-    ID       INT PRIMARY KEY,
+    ID       INT AUTO_INCREMENT PRIMARY KEY,
     PersonID INT NOT NULL,
-    FOREIGN KEY (PersonID) REFERENCES Person (ID)
+    FOREIGN KEY (PersonID) REFERENCES Person(ID)
 );
 
 CREATE TABLE SmallGroup
 (
-    ID   INT PRIMARY KEY,
+    ID   INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE SmallGroupMember
 (
-    ID           INT PRIMARY KEY,
+    ID           INT AUTO_INCREMENT PRIMARY KEY,
     AttendeeID   INT NOT NULL,
     SmallGroupID INT NOT NULL,
-    FOREIGN KEY (AttendeeID) REFERENCES Person (ID),
-    FOREIGN KEY (SmallGroupID) REFERENCES SmallGroup (ID)
+    FOREIGN KEY (AttendeeID) REFERENCES Person(ID),
+    FOREIGN KEY (SmallGroupID) REFERENCES SmallGroup(ID)
 );
 
 CREATE TABLE SmallGroupLeader
 (
-    ID           INT PRIMARY KEY,
+    ID           INT AUTO_INCREMENT PRIMARY KEY,
     LeaderID     INT NOT NULL,
     SmallGroupID INT NOT NULL,
-    FOREIGN KEY (LeaderID) REFERENCES Person (ID),
-    FOREIGN KEY (SmallGroupID) REFERENCES SmallGroup (ID)
+    FOREIGN KEY (LeaderID) REFERENCES Person(ID),
+    FOREIGN KEY (SmallGroupID) REFERENCES SmallGroup(ID)
 );
 
 CREATE TABLE Event
 (
-    ID       INT PRIMARY KEY,
-    Name     VARCHAR(50)  NOT NULL,
+    ID       INT AUTO_INCREMENT PRIMARY KEY,
+    Name     VARCHAR(50) NOT NULL,
     Type     VARCHAR(100) NOT NULL,
-    DateTime DATETIME     NOT NULL,
+    DateTime DATETIME NOT NULL,
     Location VARCHAR(100) NOT NULL,
     Notes    VARCHAR(255)
 );
 
 CREATE TABLE Registration
 (
-    ID               INT PRIMARY KEY,
-    EventID          INT         NOT NULL,
+    ID               INT AUTO_INCREMENT PRIMARY KEY,
+    EventID          INT NOT NULL,
     AttendeeID       INT,
     LeaderID         INT,
     EmergencyContact VARCHAR(50) NOT NULL,
-    FOREIGN KEY (EventID) REFERENCES Event (ID),
-    FOREIGN KEY (AttendeeID) REFERENCES Attendee (ID),
-    FOREIGN KEY (LeaderID) REFERENCES Leader (ID),
+    FOREIGN KEY (EventID) REFERENCES Event(ID),
+    FOREIGN KEY (AttendeeID) REFERENCES Attendee(ID),
+    FOREIGN KEY (LeaderID) REFERENCES Leader(ID),
     CONSTRAINT ValidRegister CHECK ((AttendeeID IS NOT NULL) OR (LeaderID IS NOT NULL))
 );
 
 CREATE TABLE AttendanceRecord
 (
-    ID       INT PRIMARY KEY,
+    ID       INT AUTO_INCREMENT PRIMARY KEY,
     PersonID INT NOT NULL,
     EventID  INT NOT NULL,
-    FOREIGN KEY (PersonID) REFERENCES Person (ID),
-    FOREIGN KEY (EventID) REFERENCES Event (ID)
+    FOREIGN KEY (PersonID) REFERENCES Person(ID),
+    FOREIGN KEY (EventID) REFERENCES Event(ID)
 );
+
 
 
 

40a37952abbb6a2d5aff9e5331bd76e12609e242
Meghan Nugent <mnugent@westmont.edu>
2025-12-09 17:08:42 -0800

    fixed our silly error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Assuming the backend is running on port 8000
+  baseURL: 'http://127.0.0.1:8099',
 });
 
 export default api;
